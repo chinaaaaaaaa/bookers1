b78cc495182165bc62a85d8487e92e6878810928
EC2 Default User <ec2-user@ip-172-31-21-120.ap-northeast-1.compute.internal>
2022-08-09 12:29:12 +0000

    destroyの機能まで完成

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  get 'top' => 'homes#top'
+  resources :books
+  #destroy' , as: 'destroy_book'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
